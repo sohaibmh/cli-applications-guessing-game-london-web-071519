825fcd4ce31e6d7f9706fcb2e717b6d7974e7a08
Sohaib Mubashir <sohaibmh@yahoo.com>
2019-06-29 22:03:54 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -2,7 +2,11 @@
 def run_guessing_game
   a = rand 1..6
   puts "Guess your number"
-  gets.chomp
+  b = gets.chomp
+  if b == a
+    puts "You guessed the correct number!"
+  elsif 
+    puts "Sorry! The computer guess #{a}"
 end
 
 puts run_guessing_game
