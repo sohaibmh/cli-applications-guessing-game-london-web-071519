149f6c66ccf4ca1d2823d34c33381ba204264b54
Sohaib Mubashir <sohaibmh@yahoo.com>
2019-06-29 22:22:09 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -1,11 +1,11 @@
 
-def run_guessing_game
+def r
   a = rand 1...2
   a
   puts a
   puts "Guess your number"
   b = gets.chomp
-  if b == a
+  if a == b
     puts "You guessed the correct number!"
   end
 end
