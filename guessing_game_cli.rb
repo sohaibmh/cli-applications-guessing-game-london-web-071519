e3b5904be634340b248372169bb5929b1b819be6
Sohaib Mubashir <sohaibmh@yahoo.com>
2019-06-29 22:05:06 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -7,6 +7,9 @@ def run_guessing_game
     puts "You guessed the correct number!"
   elsif
     puts "Sorry! The computer guessed #{a}"
+  elsif b == "exit"
+    puts "Goodbye!"
+  end
 end
 
 puts run_guessing_game
